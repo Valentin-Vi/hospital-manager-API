9843fe0d0bedd80214b296a629aa627b2baa4073
Valentin <153557240+Valentin-Vi@users.noreply.github.com>
2025-02-04 14:42:09 -0300

    Delete backend/dist directory

--- backend/dist/util/getFalseyProperties.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = getFalsyPropertyNames;
-function getFalsyPropertyNames(obj) {
-    return Object.entries(obj)
-        .filter(([_, value]) => !value)
-        .map(([key]) => key);
-}
--- backend/dist/util/verifyAccessToken.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.verifyAccessToken = void 0;
-require("dotenv");
-const jwt = require('jsonwebtoken');
-const verifyAccessToken = (access_token) => {
-    return jwt.verify(access_token, process.env.ACCESS_JWT_SECRET);
-};
-exports.verifyAccessToken = verifyAccessToken;
